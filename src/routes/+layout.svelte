c68691069dad39b86e344cfdaa2d3d1950b51dcf
MmD <mmdj27634@gmail.com>
2023-12-17 18:08:02 +0330

    add : default styles to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="app h-full">
-  <main class="relative w-full h-full max-w-[1300px] mx-auto px-12">
+  <main class="relative w-full h-full text-sm text-base-content">
     <slot />
   </main>
 </div>

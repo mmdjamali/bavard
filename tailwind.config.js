3af5542e888794c9fdaab1a1812e438557cc9ef1
MmD <mmdj27634@gmail.com>
2023-12-17 18:12:43 +0330

    add : custom easing functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@
 export default {
   content: ["./src/**/*.{html,svelte,ts,js}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "back-in-out": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
+        // ispired by https://github.com/emilkowalski, EASE constant of vaul
+        emil: "cubic-bezier(0.32, 0.72, 0, 1)",
+      },
+    },
   },
   /** @satisfies {import('daisyui').Config} */
   daisyui: {
     themes: ["light"],
   },
   plugins: [
-    require("tailwindcss-animate"),
     require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
     require("daisyui"),
   ],
 };

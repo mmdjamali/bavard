4061ca33e321502af761f401da8e01c45d63831a
MmD <mmdj27634@gmail.com>
2023-12-18 23:27:55 +0330

    add : icon component

---
+++ src/lib/components/icon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { cn } from "$lib/utils";
+
+  let className = "";
+
+  export { className as class };
+</script>
+
+<i class={cn("inline-grid place-items-center font-normal", className)}></i>

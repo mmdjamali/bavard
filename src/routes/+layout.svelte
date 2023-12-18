ec6d8d3dd00b5378690e24d5307af2b69015fab8
MmD <mmdj27634@gmail.com>
2023-12-18 23:31:49 +0330

    use provider component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
+  import Providers from "$lib/components/providers.svelte";
   import "./styles.css";
 </script>
 
 <div class="app h-full">
   <main class="relative w-full h-full text-sm text-base-content">
-    <slot />
+    <Providers>
+      <slot />
+    </Providers>
   </main>
 </div>

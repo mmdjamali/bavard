e073f3eb5d5ddda720dc89097d3a8e2ec1d9ad7c
MmD <mmdj27634@gmail.com>
2023-12-19 21:22:00 +0330

    add : timeFormater function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,28 @@ import { twMerge, type ClassNameValue } from "tailwind-merge"
 import { clsx } from "clsx"
 
 export const cn = (...inputs: ClassNameValue[]) => clsx(twMerge(inputs))
+
+export const timeFormater = (
+    date: Date
+) => {
+    const now = new Date()
+
+    let time = (now.getTime() - date.getTime()) / 1000
+
+    if (time <= 60) return (`${Math.floor(time)}s`)
+    time = time / 60
+
+    if (time <= 60) return (`${Math.floor(time)}min`)
+
+    time = time / 60
+    if (time <= 24) return (`${Math.floor(time)}h`)
+
+    time = time / 24
+    if (time <= 30) return (`${Math.floor(time) + (Math.floor(time) === 1 ? "day" : "days")}`)
+
+    time = time / 30
+    if (time <= 12) return (`${Math.floor(time)}mo`)
+
+    time = time / 12
+    return (`${Math.floor(time)}y`)
+}

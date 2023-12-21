7a6b4a3a8da2c555f06ac827363c010da5796df0
MmD <mmdj27634@gmail.com>
2023-12-21 16:11:11 +0330

    use context provider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
   import Providers from "$lib/components/providers.svelte";
+  import ContextProvider from "$lib/contexts/context-provider.svelte";
   import "./styles.css";
 </script>
 
 <div class="h-fit min-h-[100svh] relative text-sm text-base-content">
   <Providers>
-    <slot />
+    <ContextProvider>
+      <slot />
+    </ContextProvider>
   </Providers>
 </div>

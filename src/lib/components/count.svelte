38d4b0566be5ec49690c45178a396ebea294e36d
MmD <mmdj27634@gmail.com>
2023-12-21 16:17:24 +0330

    add : component for post's like,repost,... counts

---
+++ src/lib/components/count.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value: string;
+
+  const count = Number.parseInt(value);
+
+  function formatLargeNumber(num: number) {
+    const formatter = new Intl.NumberFormat("en-US", {
+      notation: "compact",
+      compactDisplay: "short",
+      maximumFractionDigits: 1,
+    });
+
+    return formatter.format(num);
+  }
+</script>
+
+{#if count > 0}
+  <span class="text-xs absolute left-[calc(100%_-_4px)] my-auto">
+    {formatLargeNumber(count)}
+  </span>
+{/if}

21d75c6820381ab283bf47c142f4a71105ddb71c
MmD <mmdj27634@gmail.com>
2023-12-21 16:15:51 +0330

    change with auth to with profile

---
+++ src/lib/components/with-profile.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { browser } from "$app/environment";
+  import { getProfileContext } from "$lib/contexts/profile/profile-context";
+
+  const profile = getProfileContext();
+
+  $: if (
+    browser &&
+    !$profile.isLoading &&
+    $profile.data?.user?.id &&
+    !$profile.data.profile?.id
+  ) {
+    goto("/auth/profile");
+  }
+  $: console.log($profile.data);
+</script>
+
+{#if $profile.isLoading}
+  <div class="grid place-items-center w-full h-full my-auto">
+    <span class="loading loading-dots ease-emil text-base-content" />
+  </div>
+{:else if $profile.isError}
+  <p>{$profile.error}</p>
+{:else}
+  <slot />
+{/if}

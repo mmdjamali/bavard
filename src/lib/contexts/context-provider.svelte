9b2aee55a05f9279f35c07b92a33d450fdd7f81e
MmD <mmdj27634@gmail.com>
2023-12-21 16:10:32 +0330

    add : gloabl context provider

---
+++ src/lib/contexts/context-provider.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { setCreatedPostsContext } from "./created-posts/created-posts-context";
+  import { setProfileContext } from "./profile/profile-context";
+
+  setCreatedPostsContext();
+  setProfileContext();
+</script>
+
+<slot />

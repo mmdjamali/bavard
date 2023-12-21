689b5a77750c7c0fe49439f7674be141821fd093
MmD <mmdj27634@gmail.com>
2023-12-21 19:10:21 +0330

    add : replying to component

---
+++ src/lib/components/post/replying-to.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { PUBLIC_BACKEND_URL } from "$env/static/public";
+  import type { ApiResponse } from "$lib/types/api";
+  import type {
+    PostEntity,
+    PostWithParent,
+    ProfileEntity,
+  } from "$lib/types/entity";
+  import { createQuery } from "@tanstack/svelte-query";
+
+  export let data: PostEntity & PostWithParent;
+
+  const parentAuthor = createQuery<ProfileEntity | null>({
+    queryKey: [{ profile: data.created_by }],
+    queryFn: async () => {
+      const res: ApiResponse<{
+        profile: null | ProfileEntity;
+      }> = await fetch(
+        PUBLIC_BACKEND_URL + "/api/profile/id/" + data.created_by,
+      ).then((res) => res?.json());
+
+      if (!res?.success) return null;
+
+      return res.data?.profile ?? null;
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    staleTime: 1000 * 10,
+  });
+</script>
+
+{#if $parentAuthor.data?.username}
+  <div class="text-base-content/75">
+    replying to <a
+      href="/profile/{$parentAuthor.data?.username}"
+      class="link link-hover link-primary">@{$parentAuthor.data?.username}</a
+    >
+  </div>
+{/if}

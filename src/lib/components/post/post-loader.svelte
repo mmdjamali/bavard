968d286ce317bac67d36298ad600eafb0f99a174
MmD <mmdj27634@gmail.com>
2023-12-26 15:03:11 +0330

    add : post skeleton component

---
+++ src/lib/components/post/post-loader.svelte
@@ -0,0 +1,39 @@
+<div class="grid grid-cols-[40px_1fr] gap-3 px-4 border-b border-base-300">
+  <div class="w-full relative h-full py-3 shrink-0">
+    <div class="w-full relative">
+      <div
+        class="w-full aspect-square rounded-full inline-grid place-items-center bg-base-300 animate-pulse"
+      />
+    </div>
+  </div>
+
+  <div class="flex relative flex-col w-full">
+    <div class="flex w-full justify-between text-sm pt-3 mt-0.5">
+      <div class="flex items-center gap-2">
+        <span class="w-28 h-3 bg-base-300 animate-pulse" />
+      </div>
+    </div>
+
+    <div class="flex flex-col relative w-full gap-2 mt-2">
+      <span class="grid w-[75%] h-4 bg-base-300 animate-pulse" />
+      <span class="grid w-[50%] h-4 bg-base-300 animate-pulse" />
+    </div>
+
+    <div class="w-full relative flex justify-between py-2 gap-4">
+      <div class="flex items-center justify-between w-full relative">
+        <div
+          class="w-8 h-8 bg-base-300 rounded-full animate-pulse ease-emil"
+        ></div>
+        <div
+          class="w-8 h-8 bg-base-300 rounded-full animate-pulse ease-emil"
+        ></div>
+        <div
+          class="w-8 h-8 bg-base-300 rounded-full animate-pulse ease-emil"
+        ></div>
+        <div
+          class="w-8 h-8 bg-base-300 rounded-full animate-pulse ease-emil"
+        ></div>
+      </div>
+    </div>
+  </div>
+</div>

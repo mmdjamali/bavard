6534e301aa1b9b6773b7482b2d178a8dc59dfcf9
MmD <mmdj27634@gmail.com>
2023-12-26 20:34:08 +0330

    add : loader for initial javascript load

---
+++ src/lib/components/loader.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  let loading = true;
+
+  onMount(() => {
+    loading = false;
+  });
+</script>
+
+{#if loading}
+  <div class="w-full grid place-items-center bg-base-100 h-[100svh]">
+    <img
+      alt="logo"
+      src="/logo.svg"
+      class="w-16 h-16 duration-300 ease-emil animate-in slide-in-from-bottom-full"
+    />
+  </div>
+{:else}
+  <slot />
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script>
+  import Loader from "$lib/components/loader.svelte";
   import Providers from "$lib/components/providers.svelte";
   import ContextProvider from "$lib/contexts/context-provider.svelte";
   import "./styles.css";
 </script>
 
-<div class="h-fit min-h-[100svh] relative text-sm text-base-content">
-  <Providers>
-    <ContextProvider>
-      <slot />
-    </ContextProvider>
-  </Providers>
-</div>
+<Loader>
+  <div class="h-fit min-h-[100svh] relative text-sm text-base-content">
+    <Providers>
+      <ContextProvider>
+        <slot />
+      </ContextProvider>
+    </Providers>
+  </div>
+</Loader>
